106660aa9f2d1b47ca044b923c31d3ed2666135c
jhngolan <joparsip@gmail.com>
2016-10-05 01:11:56 +0700

    commit 1

---
+++ looping.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+
+
+int main ()
+{
+
+ cout << "Program Perulangan 1-100";
+ cout << "=========================";
+  	
+ 
+ for ( n=1 ; n<=100 ; n++ )
+ 
+ {
+ 		cout << "Hasil Keluaran ===>" <<  n;
+
+ }
+
+  cout <<" \n ";
+  
+  return 0;
+} 
